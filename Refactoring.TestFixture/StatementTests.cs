7067e53ff3929712495ea1a8a045f4a47c0c3001
Lindsay Spencer <Lindsay.Spencer@logistyx.com>
2021-02-14 09:28:24 -0600

    Initial bill statement program with tests.

---
+++ Refactoring.TestFixture/StatementTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata;
+using Xunit;
+
+namespace Refactoring.TestFixture
+{
+    public class StatementTests
+    {
+        [Fact]
+        public void ShouldReturnValidBill()
+        {
+            var expected =
+                "Statement for BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits";
+            var plays = new Program.Plays()
+            {
+                hamlet = new Program.Play {Name = "Hamlet", Type = "tragedy"},
+                aslike = new Program.Play {Name = "As You Like It", Type = "comedy"},
+                othello = new Program.Play {Name = "Othello", Type = "tragedy"}
+            };
+
+            var x = new Program.Performance {PlayId = "hamlet", Audience = 55};
+            var y = new Program.Performance { PlayId = "aslike", Audience = 35 };
+            var z = new Program.Performance { PlayId = "othello", Audience = 40 };
+
+            Program.Invoice[] invoices = new Program.Invoice[1];
+            invoices[0] = new Program.Invoice
+            {
+                Customer = "BigCo",
+                Performances = new Program.Performance[3]
+            };
+            invoices[0].Performances[0] = x;
+            invoices[0].Performances[1] = y;
+            invoices[0].Performances[2] = z;
+
+            var actual = Program.Statement(invoices[0], plays);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- Refactoring/Program.cs
+++ Refactoring/Program.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Refactoring
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var plays = JsonConvert.DeserializeObject<Plays>(File.ReadAllText(@"external\plays.json"));
+            var invoices = JsonConvert.DeserializeObject<Invoice[]>(File.ReadAllText(@"external\invoices.json"));
+            Console.Write(Statement(invoices[0], plays));
+        }
+
+        public class Plays
+        {
+            public Play hamlet { get; set; }
+            public Play aslike { get; set; }
+            public Play othello { get; set; }
+        }
+
+        public class Play
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+        }
+
+        public class Performance
+        {
+            public string PlayId { get; set; }
+            public int Audience { get; set; }
+        }
+
+        public class Invoice
+        {
+            public string Customer { get; set; }
+            public Performance[] Performances { get; set; }
+        }
+
+        public static string Statement(Invoice invoice, Plays plays)
+        {
+            double totalAmount = 0;
+            double volumeCredits = 0;
+            var result = $"Statement for {invoice.Customer}\n";
+            CultureInfo culture = new CultureInfo("en-US");
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = (Play) plays.GetType().GetProperty(perf.PlayId)?.GetValue(plays, null);
+                var thisAmount = 0;
+
+                switch (play.Type)
+                {
+                    case "tragedy":
+                        thisAmount = 40000;
+                        if(perf.Audience > 30)
+                        {
+                            thisAmount += 1000 * (perf.Audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        thisAmount = 30000;
+                        if (perf.Audience > 20)
+                        {
+                            thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        }
+
+                        thisAmount += 300 * perf.Audience;
+                        break;
+                    default:
+                        throw new Exception($"unknown type: {play.Type}");
+                }
+
+                // add volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCredits += Math.Floor(perf.Audience / (double) 5);
+
+                // print line for this order
+                result += $"  {play.Name}: {(thisAmount / 100).ToString("C", culture)} ({perf.Audience} seats)\n";
+                totalAmount += thisAmount;
+            }
+
+            result += $"Amount owed is {(totalAmount / 100).ToString("C", culture)}\n";
+            result += $"You earned {volumeCredits} credits";
+            
+            return result;
         }
     }
 }
