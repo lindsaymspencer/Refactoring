02eb39304cc0f842001e2617052ac77e0daf1bc1
Lindsay Spencer <Lindsay.Spencer@logistyx.com>
2021-02-16 10:55:39 -0600

    Extract Function for RenderPlainText.

--- Refactoring/Program.cs
+++ Refactoring/Program.cs
@@ -42,57 +42,70 @@ namespace Refactoring
 
         public static string Statement(Invoice invoice, Plays plays)
         {
-            var result = $"Statement for {invoice.Customer}\n";
-            foreach (var perf in invoice.Performances)
-            {
-                result += $"  {PlayFor(perf).Name}: {Usd(AmountFor(perf))} ({perf.Audience} seats)\n";
-            }
-            result += $"Amount owed is {Usd(TotalAmount())}\n";
-            result += $"You earned {TotalVolumeCredits()} credits";
-            return result;
-        
-            double TotalAmount() => invoice.Performances.Aggregate<Performance, double>(0, (current, perf) => current + AmountFor(perf));
-            double TotalVolumeCredits() => invoice.Performances.Sum(VolumeCreditsFor);
-            string Usd(double aNumber) => (aNumber / 100).ToString("c", new CultureInfo("en-US"));
-            double VolumeCreditsFor(Performance aPerformance)
-        {
-            double result = 0;
-            result += Math.Max(aPerformance.Audience - 30, 0);
-            if ("comedy" == PlayFor(aPerformance).Type) result += Math.Floor(aPerformance.Audience / (double)5);
-            return result;
+            return RenderPlainText(invoice, plays);
         }
+
+        private static string RenderPlainText(Invoice invoice, Plays plays)
+        {
             Play PlayFor(Performance aPerformance)
-        {
-            return (Play)plays.GetType().GetProperty(aPerformance.PlayId)?.GetValue(plays, null);
-        }
-            int AmountFor(Performance aPerformance)
-        {
-            int result;
-            switch (PlayFor(aPerformance).Type)
             {
-                case "tragedy":
-                    result = 40000;
-                    if (aPerformance.Audience > 30)
-                    {
-                        result += 1000 * (aPerformance.Audience - 30);
-                    }
-
-                    break;
-                case "comedy":
-                    result = 30000;
-                    if (aPerformance.Audience > 20)
-                    {
-                        result += 10000 + 500 * (aPerformance.Audience - 20);
-                    }
-
-                    result += 300 * aPerformance.Audience;
-                    break;
-                default:
-                    throw new Exception($"unknown type: {PlayFor(aPerformance).Type}");
+                return (Play) plays.GetType().GetProperty(aPerformance.PlayId)?.GetValue(plays, null);
             }
 
-            return result;
-        }
+            string Usd(double aNumber) => (aNumber / 100).ToString("c", new CultureInfo("en-US"));
+
+            int AmountFor(Performance aPerformance)
+            {
+                int result;
+                switch (PlayFor(aPerformance).Type)
+                {
+                    case "tragedy":
+                        result = 40000;
+                        if (aPerformance.Audience > 30)
+                        {
+                            result += 1000 * (aPerformance.Audience - 30);
+                        }
+
+                        break;
+                    case "comedy":
+                        result = 30000;
+                        if (aPerformance.Audience > 20)
+                        {
+                            result += 10000 + 500 * (aPerformance.Audience - 20);
+                        }
+
+                        result += 300 * aPerformance.Audience;
+                        break;
+                    default:
+                        throw new Exception($"unknown type: {PlayFor(aPerformance).Type}");
+                }
+
+                return result;
+            }
+
+            double TotalAmount() =>
+                invoice.Performances.Aggregate<Performance, double>(0, (current, perf) => current + AmountFor(perf));
+
+            double VolumeCreditsFor(Performance aPerformance)
+            {
+                double result = 0;
+                result += Math.Max(aPerformance.Audience - 30, 0);
+                if ("comedy" == PlayFor(aPerformance).Type) result += Math.Floor(aPerformance.Audience / (double) 5);
+                return result;
+            }
+
+            double TotalVolumeCredits() => invoice.Performances.Sum(VolumeCreditsFor);
+            {
+                var result = $"Statement for {invoice.Customer}\n";
+                foreach (var perf in invoice.Performances)
+                {
+                    result += $"  {PlayFor(perf).Name}: {Usd(AmountFor(perf))} ({perf.Audience} seats)\n";
+                }
+
+                result += $"Amount owed is {Usd(TotalAmount())}\n";
+                result += $"You earned {TotalVolumeCredits()} credits";
+                return result;
+            }
         }
     }
 }
